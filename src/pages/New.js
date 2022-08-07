db8fad63cf402fda36582fe20d2e3a36a20057ee
iamoki <pureaoh@gmail.com>
2022-08-07 21:02:46 +0900

    ✅ 일기 작성하기 구현

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,9 @@
+import DiaryEditor from '../components/DiaryEditor';
+
 const New = () => {
   return (
     <div>
-      <h1>New</h1>
-      <p>여기는 일기 작성페이지</p>
+      <DiaryEditor />
     </div>
   );
 };
